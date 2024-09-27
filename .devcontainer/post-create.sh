824b781683b427ce56f898028202612ca4863b34
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-27 02:28:35 +0000

    Initial commit

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+sudo apt-get update
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+sudo chown vscode:vscode /workspaces
+sudo chown -R vscode:vscode /workspaces/xiangting-py
+
+python3 -m venv .venv # for `maturin develop`
+uv sync
