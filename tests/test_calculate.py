b1e03ad60e306109ee2b6dd3c68ffb9411c731af
Apricot-S <mahjong_apricot@outlook.jp>
2025-06-07 08:50:07 +0000

    test: Enhance Readability by Parsing Hand Tiles from Tenhou Format in Unit Tests

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -5,6 +5,7 @@
 import re
 
 import pytest
+from mahjong.tile import TilesConverter
 
 from xiangting import (
     ClaimedTilePosition,
@@ -15,45 +16,25 @@ from xiangting import (
 
 
 def test_calculate_replacement_number_standard_tenpai() -> None:
-    bingpai = [
-        1, 1, 1, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 1, 1, 1, # s
-        2, 2, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("123m456p789s1122z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_shisanyao_tenpai() -> None:
-    bingpai = [
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # m
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # p
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # s
-        1, 1, 1, 1, 1, 1, 1, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("19m19p19s1234567z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_qiduizi_tenpai() -> None:
-    bingpai = [
-        2, 0, 0, 0, 0, 0, 0, 2, 0, # m
-        0, 1, 0, 0, 0, 0, 0, 2, 0, # p
-        0, 0, 0, 0, 2, 0, 0, 0, 0, # s
-        2, 0, 0, 0, 0, 0, 2, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("1188m288p55s1177z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_with_meld() -> None:
-    bingpai = [
-        1, 1, 1, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        0, 1, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("123m456p2z")
     replacement_number_1 = calculate_replacement_number(bingpai, None)
     assert replacement_number_1 == 1
 
@@ -71,12 +52,7 @@ def test_calculate_replacement_number_with_meld() -> None:
 
 
 def test_calculate_replacement_number_empty_bingpai() -> None:
-    bingpai = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        0, 0, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("")
 
     error_msg = "hand is empty"
     with pytest.raises(ValueError, match=error_msg):
@@ -84,12 +60,7 @@ def test_calculate_replacement_number_empty_bingpai() -> None:
 
 
 def test_calculate_replacement_number_invalid_meld() -> None:
-    bingpai = [
-        1, 1, 1, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        0, 1, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("123m456p2z")
 
     fulu_mianzi_list: list[FuluMianzi | None] = [
         FuluMianzi.Kezi(34),
@@ -108,122 +79,67 @@ def test_calculate_replacement_number_invalid_meld() -> None:
 
 
 def test_calculate_replacement_number_lack_isolated_tile_13_4333() -> None:
-    bingpai = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 3, 3, 3, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("1111222333444z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 2  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_13_4432i() -> None:
-    bingpai = [
-        2, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 3, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("11m11112222333z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 3  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_13_4432ii() -> None:
-    bingpai = [
-        0, 1, 1, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 3, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("23m11112222333z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 3  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_13_4441() -> None:
-    bingpai = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 4, 1, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("1111222233334z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 4  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_14_4433() -> None:
-    bingpai = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 3, 3, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("11112222333444z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 2  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_14_4442i() -> None:
-    bingpai = [
-        2, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 4, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("11m111122223333z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 3  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_lack_isolated_tile_14_4442ii() -> None:
-    bingpai = [
-        0, 1, 1, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        4, 4, 4, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("23m111122223333z")
     replacement_number = calculate_replacement_number(bingpai, None)
     assert replacement_number == 3  # noqa: PLR2004
 
 
 def test_calculate_replacement_number_3_player_standard_tenpai() -> None:
-    bingpai = [
-        3, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 1, 1, 1, # s
-        2, 2, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("111m456p789s1122z")
     replacement_number = calculate_replacement_number_3_player(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_3_player_shisanyao_tenpai() -> None:
-    bingpai = [
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # m
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # p
-        1, 0, 0, 0, 0, 0, 0, 0, 1, # s
-        1, 1, 1, 1, 1, 1, 1, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("19m19p19s1234567z")
     replacement_number = calculate_replacement_number_3_player(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_3_player_qiduizi_tenpai() -> None:
-    bingpai = [
-        2, 0, 0, 0, 0, 0, 0, 0, 2, # m
-        0, 1, 0, 0, 0, 0, 0, 2, 0, # p
-        0, 0, 0, 0, 2, 0, 0, 0, 0, # s
-        2, 0, 0, 0, 0, 0, 2, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("1199m288p55s1177z")
     replacement_number = calculate_replacement_number_3_player(bingpai, None)
     assert replacement_number == 1
 
 
 def test_calculate_replacement_number_3_player_with_meld() -> None:
-    bingpai = [
-        3, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 1, 1, 1, # s
-        0, 1, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("111m456p789s2z")
     replacement_number_1 = calculate_replacement_number_3_player(bingpai, None)
     assert replacement_number_1 == 1
 
@@ -241,12 +157,7 @@ def test_calculate_replacement_number_3_player_with_meld() -> None:
 
 
 def test_calculate_replacement_number_3_player_empty_bingpai() -> None:
-    bingpai = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        0, 0, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("")
 
     error_msg = "hand is empty"
     with pytest.raises(ValueError, match=error_msg):
@@ -254,12 +165,7 @@ def test_calculate_replacement_number_3_player_empty_bingpai() -> None:
 
 
 def test_calculate_replacement_number_3_player_invalid_meld() -> None:
-    bingpai = [
-        3, 0, 0, 0, 0, 0, 0, 0, 0, # m
-        0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-        0, 0, 0, 0, 0, 0, 0, 0, 0, # s
-        0, 1, 0, 0, 0, 0, 0, # z
-    ]  # fmt: skip
+    bingpai = TilesConverter.one_line_string_to_34_array("111m456p2z")
 
     fulu_mianzi_list: list[FuluMianzi | None] = [
         FuluMianzi.Kezi(1),
