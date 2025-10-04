9263e98b2631a049a60ad2ea9a3643a802e9ed03
Apricot-S <mahjong_apricot@outlook.jp>
2025-10-04 10:23:35 +0000

    docs: Change examples

--- examples/melds.py
+++ examples/melds.py
@@ -8,8 +8,8 @@ from xiangting import (
     calculate_replacement_number,
 )
 
-# 123m1z (3 melds)
-hand_4 = [
+# 123m1z
+hand = [
     1, 1, 1, 0, 0, 0, 0, 0, 0, # m
     0, 0, 0, 0, 0, 0, 0, 0, 0, # p
     0, 0, 0, 0, 0, 0, 0, 0, 0, # s
@@ -21,11 +21,10 @@ melds = [
     FuluMianzi.Shunzi(12, ClaimedTilePosition.LOW),
     FuluMianzi.Gangzi(24),
     FuluMianzi.Kezi(27),
-    None,
 ]
 
-replacement_number_wo_melds = calculate_replacement_number(hand_4, None)
+replacement_number_wo_melds = calculate_replacement_number(hand, None)
 assert replacement_number_wo_melds == 1
 
-replacement_number_w_melds = calculate_replacement_number(hand_4, melds)
+replacement_number_w_melds = calculate_replacement_number(hand, melds)
 assert replacement_number_w_melds == 2
