b231a9de2ea4960d94a5a6160bd2a49356680780
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-21 04:08:05 +0000

    docs: Update examples

--- examples/three_player.py
+++ examples/three_player.py
@@ -2,10 +2,7 @@
 # SPDX-License-Identifier: MIT
 # This file is part of https://github.com/Apricot-S/xiangting-py
 
-from xiangting import (
-    calculate_replacement_number,
-    calculate_replacement_number_3_player,
-)
+from xiangting import PlayerCount, calculate_replacement_number
 
 # 1111m111122233z
 hand = [
@@ -15,8 +12,8 @@ hand = [
     4, 3, 2, 0, 0, 0, 0, # z
 ]  # fmt: skip
 
-replacement_number_4p = calculate_replacement_number(hand, None)
+replacement_number_4p = calculate_replacement_number(hand, PlayerCount.FOUR)
 assert replacement_number_4p == 2
 
-replacement_number_3p = calculate_replacement_number_3_player(hand, None)
+replacement_number_3p = calculate_replacement_number(hand, PlayerCount.THREE)
 assert replacement_number_3p == 3
