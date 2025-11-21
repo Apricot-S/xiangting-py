dc30076bec27623864f5602a62d6275e666e605d
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-21 16:26:00 +0000

    docs: Improve docstrings for calculate_necessary_tiles() (WIP)

--- src/necessary_tiles.rs
+++ src/necessary_tiles.rs
@@ -21,15 +21,41 @@ use pyo3::prelude::*;
 ///     ValueError: If the hand is invalid.
 ///
 /// Examples:
-///     >>> # 123m456p789s11222z
+///     >>> # 199m146779p12s246z
 ///     >>> hand = [
-///     ...     1, 1, 1, 0, 0, 0, 0, 0, 0, # m
-///     ...     0, 0, 0, 1, 1, 1, 0, 0, 0, # p
-///     ...     0, 0, 0, 0, 0, 0, 1, 1, 1, # s
-///     ...     2, 3, 0, 0, 0, 0, 0, # z
+///     ...     1, 0, 0, 0, 0, 0, 0, 0, 2, # m
+///     ...     1, 0, 0, 1, 0, 1, 2, 0, 1, # p
+///     ...     1, 1, 0, 0, 0, 0, 0, 0, 0, # s
+///     ...     0, 1, 0, 1, 0, 1, 0, # z
 ///     ... ]
-///     >>> calculate_replacement_number(hand, None)
-///     0
+///     >>> rn, nt = calculate_necessary_tiles(hand, PlayerCount.FOUR)
+///     >>> rn
+///     5
+///     >>> # 1239m123456789p1239s1234567z
+///     >>> nt == 0b1111111_100000111_111111111_100000111
+///     True
+///
+///     In three-player mahjong, the tiles from 2m (二萬) to 8m (八萬) are not used.
+///
+///     >>> # 1111m111122233z
+///     >>> hand = [
+///     ...     4, 0, 0, 0, 0, 0, 0, 0, 0, # m
+///     ...     0, 0, 0, 0, 0, 0, 0, 0, 0, # p
+///     ...     0, 0, 0, 0, 0, 0, 0, 0, 0, # s
+///     ...     4, 3, 2, 0, 0, 0, 0, # z
+///     ... ]
+///     >>> rn_4p, nt_4p = calculate_necessary_tiles(hand, PlayerCount.FOUR)
+///     >>> rn_4p
+///     2
+///     >>> # 23m
+///     >>> nt_4p == 0b0000000_000000000_000000000_000000110
+///     True
+///     >>> rn_3p, nt_3p = calculate_necessary_tiles(hand, PlayerCount.THREE)
+///     >>> rn_3p
+///     3
+///     >>> # 9m123456789p123456789s34567z
+///     >>> nt_3p == 0b1111100_111111111_111111111_100000000
+///     True
 ///
 #[pyfunction]
 #[pyo3(signature = (bingpai, player_count))]
