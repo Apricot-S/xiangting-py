935cfede0c5613284ba6ed3e4bbc6d2a2f9f1a84
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-21 06:37:18 +0000

    docs: Add examples

--- examples/three_player.py
+++ examples/three_player.py
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: MIT
 # This file is part of https://github.com/Apricot-S/xiangting-py
 
-from xiangting import PlayerCount, calculate_replacement_number
+from xiangting import (
+    PlayerCount,
+    calculate_necessary_tiles,
+    calculate_unnecessary_tiles,
+)
 
 # 1111m111122233z
 hand = [
@@ -12,8 +16,15 @@ hand = [
     4, 3, 2, 0, 0, 0, 0, # z
 ]  # fmt: skip
 
-replacement_number_4p = calculate_replacement_number(hand, PlayerCount.FOUR)
-assert replacement_number_4p == 2
+rn_4p, nt_4p = calculate_necessary_tiles(hand, PlayerCount.FOUR)
+_, ut_4p = calculate_unnecessary_tiles(hand, PlayerCount.FOUR)
+assert rn_4p == 2
+assert nt_4p == 0b0000000_000000000_000000000_000000110  # 23m
+assert ut_4p == 0b0000001_000000000_000000000_000000000  # 1z
 
-replacement_number_3p = calculate_replacement_number(hand, PlayerCount.THREE)
-assert replacement_number_3p == 3
+rn_3p, nt_3p = calculate_necessary_tiles(hand, PlayerCount.THREE)
+_, ut_3p = calculate_unnecessary_tiles(hand, PlayerCount.THREE)
+assert rn_3p == 3
+# 9m123456789p123456789s34567z
+assert nt_3p == 0b1111100_111111111_111111111_100000000
+assert ut_3p == 0b0000001_000000000_000000000_000000001  # 1m1z
